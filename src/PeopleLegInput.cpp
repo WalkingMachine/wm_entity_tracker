a69e1eb61e0e5b7a4b9aab4c4c007fd0ba131fa2
philippe <philippe117@live.fr>
2019-05-03 14:26:22 -0400

    utilise les bon id pour lassignation des jambes

--- src/PeopleLegInput.cpp
+++ src/PeopleLegInput.cpp
@@ -29,11 +29,11 @@ void PeopleLegInput::peopleLegCallback(PositionMeasurementArray legArray) {
         if (legs.reliability > 0) {
             PerceivedEntity en;
 
-            en.legID = legs.object_id;
+            en.addLegsID(legs.object_id);
 
             // We look for a match in previously detected legs
             for (auto & entity : tracker().entities()){
-                if (entity.checkLegsID(en.legID)) {
+                if (entity.checkLegsID(legs.object_id)) {
                     // If a match is found, we force the entity ID.
                     en.ID = entity.ID;
                     break;
